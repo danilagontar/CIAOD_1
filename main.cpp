d629463ae9348a4a35da8ba7aba9ab8bf9bc94f3
danil <danilagontar@gmail.com>
2026-09-21 23:41:13 +0300

    refactor: написана структура main и добавлен файл cars100

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+struct CarRecord {
+    char plate[12];
+    char brand[24];
+    char owner[32];
+};
+
+int main() {
+
+    return 0;
+}
