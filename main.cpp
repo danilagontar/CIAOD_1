c52393d0a6be82b39dfd000ae0d6f7321191158d
danil <danilagontar@gmail.com>
2026-09-23 22:31:37 +0300

    feat: написана функция printBinaryFile, написан case 1 для вывода бинарного файла

--- main.cpp
+++ main.cpp
@@ -33,19 +33,51 @@ bool textToBinary(const string& textPath, const string& binPath) {
     return true;
 }
 
+void printBinaryFile(const string& binPath) {
+    ifstream in(binPath, ios::binary);
+    if (in.fail()) {
+        cerr << "Error opening files" << endl;
+        return;
+
+    }
+    carRecord record;
+    int count = 0;
+
+    while (in.read(reinterpret_cast<char*>(&record), sizeof(carRecord))) {
+        count++;
+        cout << count << ") "
+             << record.plate << " | "
+             << record.brand << " | "
+             << record.owner << endl;
+    }
+
+    in.close();
+}
+
 int main() {
     char menu;
     while (true) {
-        cout << ""
-        << ""
-        << "";
+        cout << "1. Convert the text to binary\n"
+        << "0. Exit\n";
+        cout << "Enter your choice: ";
         cin >> menu;
 
         switch (menu) {
-            case '1':
-            case '0':
+            case '1': {
+                string textFile = "cars100.txt";
+                string binFile = "cars100.bin";
+                if (textToBinary(textFile, binFile)) {
+                    cout << "Binary File Successfully Converted" << endl;
+                    cout << "The contents of the binary file" << endl;
+                    printBinaryFile(binFile);
+                    cout << endl;
+                }
+                break;
+            }
+            case '0': {
                 cout << "End Program";
                 return 0;
+            }
             default:
                 cout << "----Incorrect input!----";
         }
