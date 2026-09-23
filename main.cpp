32c55665f2c22e8e94efacf44f22724403977552
danil <danilagontar@gmail.com>
2026-09-23 22:19:48 +0300

    feat: написана функция textToBinary, сделано меню с 2 параметрами

--- main.cpp
+++ main.cpp
@@ -5,13 +5,49 @@
 
 using namespace std;
 
-struct CarRecord {
+struct carRecord {
     char plate[12];
     char brand[24];
     char owner[32];
 };
 
-int main() {
+bool textToBinary(const string& textPath, const string& binPath) {
+    ifstream in(textPath);
+    ofstream out(binPath, ios::binary);
+    if (in.fail() || out.fail()) {
+        cerr << "Error opening files" << endl;
+        return false;
+    }
+    carRecord record;
+    string tempPlate, tempBrand, tempOwner;
+    while (in >> tempPlate >> tempBrand >> tempOwner) {
+        memset(&record, 0, sizeof(record));
+        strncpy(record.plate, tempPlate.c_str(), sizeof(record.plate) - 1);
+        strncpy(record.brand, tempBrand.c_str(), sizeof(record.brand) - 1);
+        strncpy(record.owner, tempOwner.c_str(), sizeof(record.owner) - 1);
+        out.write(reinterpret_cast<char*>(&record), sizeof(carRecord));
+    }
+    in.close();
+    out.close();
 
-    return 0;
+    return true;
+}
+
+int main() {
+    char menu;
+    while (true) {
+        cout << ""
+        << ""
+        << "";
+        cin >> menu;
+
+        switch (menu) {
+            case '1':
+            case '0':
+                cout << "End Program";
+                return 0;
+            default:
+                cout << "----Incorrect input!----";
+        }
+    }
 }
